9a85ce08ed615ef67a5d1939d41db70983209af8
sby5388 <sby5388@github.com>
2018-11-05 14:15:49 +0800

    add project "two-libs"

---
+++ two-libs/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/twolib-first/first.o.d
@@ -0,0 +1,5 @@
+E:/by5388/android/demos/learnjni/two-libs/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/twolib-first/first.o: \
+  E:/by5388/android/demos/learnjni/two-libs/src/main/cpp/first.c \
+  E:/by5388/android/demos/learnjni/two-libs/src/main/cpp/first.h
+
+E:/by5388/android/demos/learnjni/two-libs/src/main/cpp/first.h:
---
+++ two-libs/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/twolib-second/second.o.d
@@ -0,0 +1,5 @@
+E:/by5388/android/demos/learnjni/two-libs/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/twolib-second/second.o: \
+  E:/by5388/android/demos/learnjni/two-libs/src/main/cpp/second.c \
+  E:/by5388/android/demos/learnjni/two-libs/src/main/cpp/first.h
+
+E:/by5388/android/demos/learnjni/two-libs/src/main/cpp/first.h:
